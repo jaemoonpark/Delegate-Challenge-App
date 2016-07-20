40902b42f9c5ad6dae1022a3f5318c3b83969463
Jaemoon <jaemoon@jaemoonpark.com>
2016-07-20 19:25:36 -0400

    Initial Commit

---
+++ Delegates ChallengeTests/Delegates_ChallengeTests.swift
@@ -0,0 +1,36 @@
+//
+//  Delegates_ChallengeTests.swift
+//  Delegates ChallengeTests
+//
+//  Created by Jaemoon Park on 7/20/16.
+//  Copyright © 2016 Jaemoon Park. All rights reserved.
+//
+
+import XCTest
+@testable import Delegates_Challenge
+
+class Delegates_ChallengeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
